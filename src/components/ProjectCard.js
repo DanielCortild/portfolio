f1be0c474c574d7e5dbaa95bcc39c2731b26bc10
DanielCortild <daniel.cortild@gmail.com>
2020-11-04 12:02:14 +0100

    Changed Cards

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -7,9 +7,9 @@ const ProjectCard = ({ image, title, description, url }) => {
   return (
     <Col xs={12} md={6} xl={4} className="projectCard">
       <Card>
+        <CardTitle><h3>{title}</h3></CardTitle>
         <CardImg top src={`images/${image}`} alt="" />
         <CardBody>
-          <CardTitle><h3>{title}</h3></CardTitle>
           <CardText dangerouslySetInnerHTML={{__html: description}}></CardText>
           <Button href={url} target="_blank">
             <div className="notPhone">Visit Website</div>
